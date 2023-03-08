e166422dc40ad737fc3e4540d5ae2bf2e35d66b1
Taufik Budi Santosa <taufikbudis@gmail.com>
2023-03-08 19:45:13 +0800

    Fundamental Python

---
+++ flowchart fundamental python.py
@@ -0,0 +1,12 @@
+"""
+Semua sintaksis dasar bahasa pemograman terdiri dari:
+1. Sekuensial: langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+# Sekuensial
+print('Ibu berkata, "pergi ke toko"')
+print('Ahmad menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beli satu botol susu, dan jika ada telur beli 6"')
+print('Ibu menjawab, "Maka Ahmad pergi ke toko"')
