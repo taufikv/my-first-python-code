76d1bbf4a222ba588f0f53a4b94dee57c4a15b99
Taufik Budi Santosa <taufikbudis@gmail.com>
2023-03-14 09:37:08 +0800

    Fundamental Python

--- flowchart fundamental python.py
+++ flowchart fundamental python.py
@@ -10,4 +10,30 @@ print('Ibu berkata, "pergi ke toko"')
 print('Ahmad menjawab, "Baik, apa yang harus saya lakukan di toko?"')
 print('Ibu menjawab, "Beli satu botol susu, dan jika ada telur beli 6"')
 print("Maka Ahmad pergi ke toko")
-print('Dan mulai ')
+print("Dan mulai")
+
+print("=" * 10)
+
+print('\nIbu berkata "Beli 1 botol susu dan 6 butir telur"')
+print('Anak menjawab "Ok"')
+print('Anak pergi ke toko')
+print('Dan mulai berbelanja')
+
+jumlah_botol_susu = 75
+jumlah_telur      = 89
+print(f'Jumlah botol susu {jumlah_botol_susu} botol')
+print(f'Jumlah Telur {jumlah_telur} butir')
+
+print('=' * 30)
+if jumlah_botol_susu > 0:
+    print('anak mengecek harganya,dan ternyata uangnya cukup')
+    if jumlah_telur == 0:
+        print('anak membeli 1 botol susu')
+    else:
+        print('anak membeli 1 botol susu dan 6 butir telur')
+else:
+    print('anak tidak jadi membeli susu')
+print('=' * 30)
+
+print('\nanak pulang kerumah')
+print('Menyerahkan hasil belanjanya kepada ibu')
